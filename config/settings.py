b3772e554d1b6ef244f2b48a5b28d7c2f74d1f1f
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-06 09:52:07 +0000

    Debug se to True

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-joanne1980-taskify-jouluotmsfj.ws.codeinstitute-ide.net', 'taskify-todo-eaffb38b30eb.herokuapp.com', 'herokuapp.com']
 
