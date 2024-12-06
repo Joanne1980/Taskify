a318ce70d48139d2ea5facfddc5bff8a96897d08
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-06 14:52:18 +0000

    create form.py and add to todo

--- task/views.py
+++ task/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
+
 
 
 # Create your views here.
 def index(request):
     task = todo.objects.all()
 
-    context = {'task':task}
+    form = Todoform()
+
+    if request.method =='POST':
+        form = Todoform(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'task':task, 'form':form}
     return render(request, 'task/todo.html', context)
