5684fd853deba0058f62a6247a229717dcfa9e71
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-06 11:52:04 +0000

    Add task app, rendered view, urls and template and added urls

--- task/views.py
+++ task/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
 
 # Create your views here.
+def index(request):
+    task = todo.objects.all()
+
+    context = {'task':task}
+    return render(request, 'task/todo.html', context)
