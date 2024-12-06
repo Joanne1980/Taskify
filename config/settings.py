b5107b146ddac5560e35a5d67d89479bddcdf071
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-06 09:50:02 +0000

    error with allowed host and set debug to false for deploy to heroku

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-joanne1980-taskify-jouluotmsfj.ws.codeinstitute-ide.net', 'taskify-todo-eaffb38b30eb.herokuapp.com/', 'herokuapp.com']
+ALLOWED_HOSTS = ['8000-joanne1980-taskify-jouluotmsfj.ws.codeinstitute-ide.net', 'taskify-todo-eaffb38b30eb.herokuapp.com', 'herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net',
 'https://*.herokuapp.com']
