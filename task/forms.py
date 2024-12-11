3394cd075af01c627367bf80409d6cb3ab52ebe4
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-11 22:32:10 +0000

    Add main.html and connected each page

--- task/forms.py
+++ task/forms.py
@@ -1,11 +1,7 @@
 from django import forms
-from django.forms import ModelForm
-from .models import *
 
-class TodoForm(forms.ModelForm):
+# Reordering Form and View
 
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add in a new task'}))
 
-    class Meta:
-        model = Todo
-        fields = '__all__'
+class PositionForm(forms.Form):
+    position = forms.CharField()
