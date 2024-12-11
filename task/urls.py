2af47082d6ecfec40208ba34d0235af8f4ec582e
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-12-11 14:48:07 +0000

    Add todo edit

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TodoList, TodoDetail, TodoCreate
+from .views import TodoList, TodoDetail, TodoCreate, TodoUpdate
 
 urlpatterns = [
     path('', TodoList.as_view(), name="tasks"),
     path('todo/<int:pk>/', TodoDetail.as_view(), name='task'),
     path('create-todo/', TodoCreate.as_view(), name="task-create"),
+    path('update-todo/<int:pk>/', TodoUpdate.as_view(), name='task-update'),
+
 
 ]
