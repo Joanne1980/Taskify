4fb536ae1b13f15678abe2ac124f82c8827d6399
Sarah Joanne <joanne.clarke23@hotmail.com>
2025-02-03 09:45:10 +0000

    Add toast messages

--- task/views.py
+++ task/views.py
@@ -77,6 +77,7 @@ class TodoCreate(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TodoCreate, self).form_valid(form)
+        
 
 
 class TodoUpdate(LoginRequiredMixin, UpdateView):
